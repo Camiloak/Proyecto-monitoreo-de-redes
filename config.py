8b830291a14c8aa6ca92c738a4becfa901408812
daniel1321-estu <destupinan86@ucatolica.edu.co>
2025-05-18 14:17:18 -0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Si estamos en Render, usará PostgreSQL, si no, SQLite local
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///local.db")
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
